855d070d72de32ca43cb2790b5e021ee32513efa
yanoo_japan_com@yahoo.co.jp <admin@MacBookPro.local>
2014-05-11 20:37:15 +0900

    first commit

---
+++ HelloWorld/README.md
@@ -0,0 +1,3 @@
+Perl_HelloWorld
+===============
+Perlのプロジェクト
---
+++ HelloWorld/hello.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+# Perlのお約束
+use strict;
+use warnings;
+
+print "Hello World\n";
+
+# スカラ変数 - ひとつの値を代入できる変数
+my $str  = 'Hello'; #文字列
+my $num1 = 5; # 整数
+my $num2 = 6.7; # 少数
+
+# 変数展開
+print "$str\n";
+print "$num1\n";
+print "$num2\n";
